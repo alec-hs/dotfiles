7b1f3a284ecc040c56d4bf7ba4290b6bd09e54b5
alec-hs <6503411+alec-hs@users.noreply.github.com>
2021-06-13 03:26:14 +0100

    Basic zsh setup (#1)
    * Readme & setup changes
    
    * Fixed oh-my-zsh install
    
    * Fix zsh plugins install location
    
    * Removed surplus sudo
    
    * Added set default shell
    
    * Added missing plugin
    
    * Add missing dir

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# dotfiles
-My dotfiles repo for quick env setup. 
+# My dotfiles and OS setup
+
+## Intro
+
+My repo to setup OS and dotfiles on Linux, MacOS etc
+
+## Requirements
+
+- git
+- curl
+
+## Install
+
+1. Make sure git and curl are installed
+
+    ```bash
+    sudo apt update -y && sudo apt install git curl -y
+    ```
+
+2. Clone repo
+
+    ```bash
+    git clone https://github.com/alec-hs/dotfiles ~/.dotfiles
+    ```
+
+3. Make relevant setup file executable
+
+    ```bash
+    chmod +x ~/.dotfiles/setup/debian.sh
+    ```
+
+4. Run setup file
+
+    ```bash
+    ~/.dotfiles/setup/debian.sh
+    ```
--- setup/debian.sh
+++ setup/debian.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 # Update and install basic packages
-apt update -y && apt upgrade -y
-apt install zsh zplug net-tools -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install zsh zplug net-tools -y
 
 # Install oh-my-zsh & plugins
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
+
+# Create some dirs
+mkdir ~/.z
 
 # Delete exsting dotfiles
-rm ~/.zshrc
-rm ~/.p10k.zsh
+rm -f ~/.zshrc
+rm -f ~/.p10k.zsh
 
 # Create Symlinks
-ln -sv ~/dotfiles/zsh/.zshrc ~
-ln -sv ~/dotfiles/zsh/.p10k.zsh ~
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+
+# Set default shell
+chsh -s $(which zsh)
