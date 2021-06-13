2ffc9251fb3c7472ca4fc92b6c9bd9c697a7e3a7
alec-hs <6503411+alec-hs@users.noreply.github.com>
2021-06-13 02:56:22 +0100

    Removed surplus sudo

--- setup/debian.sh
+++ setup/debian.sh
@@ -6,11 +6,11 @@ sudo apt install zsh zplug net-tools -y
 
 # Install oh-my-zsh & plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-sudo git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
 
 # Delete exsting dotfiles
 rm ~/.zshrc
