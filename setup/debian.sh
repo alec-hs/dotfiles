fbb8c3927c29649141e29857b97b76fd0fed810b
alec-hs <6503411+alec-hs@users.noreply.github.com>
2021-06-13 03:11:19 +0100

    Added missing plugin

--- setup/debian.sh
+++ setup/debian.sh
@@ -11,6 +11,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
 
 # Delete exsting dotfiles
 rm -f ~/.zshrc
