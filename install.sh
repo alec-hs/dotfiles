c211418c21d1ed2a43c7ae11c8d5d9e53d3d80aa
alec-hs <6503411+alec-hs@users.noreply.github.com>
2021-08-05 17:13:36 +0000

    Added install script for github codespaces

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Setup file for github codespaces
+
+# Update and install basic packages
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install zsh zplug net-tools -y
+
+# Install oh-my-zsh & plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
+
+# Create z file
+touch ~/.z
+
+# Delete exsting dotfiles
+rm -f ~/.zshrc
+rm -f ~/.p10k.zsh
+
+# Create Symlinks
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+
+# Set default shell
+chsh -s $(which zsh)
