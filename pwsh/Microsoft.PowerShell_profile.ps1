d0a22194fea8e37c22b71e0fe1a922677860c9f4
alec-hs <6503411+alec-hs@users.noreply.github.com>
2021-11-04 00:36:32 +0000

    Removed unwanted PS Profile lines

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,1 @@
 oh-my-posh --init --shell pwsh --config ~/.dotfiles/oh-my-posh/alec-hs.omp.json | Invoke-Expression
-Import-Module ".\modules\coreFunctions.psm1"
-Import-Module ".\modules\installFonts.psm1"
